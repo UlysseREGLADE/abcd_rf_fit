2a72fcbb8e713e1f2b5c938a527124aa9b176752
ureglade <ulysse.reglade@alice-bob.com>
2024-06-06 20:28:39 +0200

    generalized fraction fit implementation

--- abcd_rf_fit/fraction_fit.py
+++ abcd_rf_fit/fraction_fit.py
@@ -1,61 +1,193 @@
 import numpy as np
-import sympy as sy
-from scipy.special import comb, factorial
+import matplotlib.pyplot as plt
+
+from scipy.special import comb
+from utils import complex_fit
+
+def fit_rational_fraction_pole_basis(x, y, n, poles = None, den = None):
+    if den is None:
+        den = np.ones(x.shape)
+    
+    if poles is None:
+        poles = np.linspace(np.min(x), np.max(x), n+2, dtype=complex)
+        poles = 0.5*(poles[1:] + poles[:-1])
+        poles += np.mean(np.diff(poles))*1j*(-1)**(np.arange(n+1))
+    
+    pole_mesh, x_mesh = np.meshgrid(poles, x)
+
+    x_design = (1/(x_mesh-pole_mesh)).T
+
+    xx = (x_design * np.abs(den) ** -2) @ x_design.T
+    xdyx = (y * x_design * np.abs(den) ** -2) @ x_design.T
+    xdycx = (np.conj(y) * x_design * np.abs(den) ** -2) @ x_design.T
+    xdy2x = (np.abs(y) ** 2 * x_design * np.abs(den) ** -2) @ x_design.T
+
+    up_right = np.linalg.inv(xx) @ xdyx
+    bottom_left = np.linalg.inv(xdy2x) @ xdycx
+
+    to_diag = np.zeros((2*(n+1), 2*(n+1)), dtype=complex)
+    to_diag[:n+1, n+1:] = up_right
+    to_diag[n+1:, :n+1] = bottom_left
+
+    v, w = np.linalg.eig(to_diag)
+
+    solution = w[:, np.argmin(np.abs(1 - v))]
+
+    num = x_design.T @ solution[:n+1]
+    den = x_design.T @ solution[n+1:]
+
+    return num, den
+
+def fit_rational_fraction_polynomial_basis(x, y, n, den = None):
+    if den is None:
+        den = np.ones(x.shape)
+
+    n_mesh, x_mesh = np.meshgrid(np.arange(n+1), x)
+
+    x_design = (x_mesh**n_mesh).T
+
+    xx = (x_design * np.abs(den) ** -2) @ x_design.T
+    xdyx = (y * x_design * np.abs(den) ** -2) @ x_design.T
+    xdycx = (np.conj(y) * x_design * np.abs(den) ** -2) @ x_design.T
+    xdy2x = (np.abs(y) ** 2 * x_design * np.abs(den) ** -2) @ x_design.T
+
+    up_right = np.linalg.inv(xx) @ xdyx
+    bottom_left = np.linalg.inv(xdy2x) @ xdycx
+
+    to_diag = np.zeros((2*(n+1), 2*(n+1)), dtype=complex)
+    to_diag[:n+1, n+1:] = up_right
+    to_diag[n+1:, :n+1] = bottom_left
+
+    v, w = np.linalg.eig(to_diag)
+
+    solution = w[:, np.argmin(np.abs(1 - v))]
+
+    num = x_design.T @ solution[:n+1]
+    den = x_design.T @ solution[n+1:]
+
+    return num, den, solution
+
+def convergence_criteria(array, convergance_precision, min_converged_passes):
+    array_convergence = np.diff(array)/array[:-1]
+
+    if np.prod(np.abs(array_convergence[-min_converged_passes:]) < convergance_precision):
+        return True
+    return False
+
+def iterative_solver(x, y, n, min_rec_depth=10, max_rec_depth=100, convergance_precision=0.01, min_converged_passes=5):
+    residuals = []
+
+    center = 0.5*(np.max(x) + np.min(x))
+    span = np.max(np.abs(x-center))
+    x_norm = (x-center)/span
+    
+    num, den = fit_rational_fraction_pole_basis(x_norm, y, n=n)
+    num, den, solution = fit_rational_fraction_polynomial_basis(x_norm, y, n=n, den=den)
+
+    for _ in range(max(min_rec_depth, min_converged_passes)):
+        num, den = fit_rational_fraction_pole_basis(x_norm, y, n=n, den=den)
+        num, den, solution = fit_rational_fraction_polynomial_basis(x_norm, y, n=n, den=den)
+
+        residuals.append(np.sum(np.abs(y-num/den)))
+
+    if convergence_criteria(np.array(residuals), convergance_precision, min_converged_passes):
+        return num, den, solution, residuals
+    
+    for _ in range(max(max_rec_depth - min_rec_depth, 0)):
+        num, den = fit_rational_fraction_pole_basis(x_norm, y, n=n, den=den)
+        num, den, solution = fit_rational_fraction_polynomial_basis(x_norm, y, n=n, den=den)
+
+        residuals.append(np.sum(np.abs(y-num/den)))
+
+        if convergence_criteria(np.array(residuals), convergance_precision, min_converged_passes):
+            return num, den, solution, residuals
+    
+    return num, den, solution, residuals
+
+def convert_solution(x, solution):
+    n = (len(solution) // 2) - 1
+    scalled_solution = np.copy(solution)
+    
+    center = 0.5*(np.max(x) + np.min(x))
+    span = np.max(np.abs(x-center))
+
+    scalled_solution[:n+1] /= span**np.arange(n+1)
+    scalled_solution[n+1:] /= span**np.arange(n+1)
+
+    translated_solution = np.zeros_like(solution)
+
+    for i in range(n+1):
+        num_i = 0
+        den_i = 0
+
+        for j in range(i, n + 1):
+            num_i += scalled_solution[j] * comb(j, i) * (-center)**(j - i)
+            den_i += scalled_solution[n+1+j] * comb(j, i) * (-center)**(j - i)
+        
+        translated_solution[i] = num_i
+        translated_solution[n+1+i] = den_i
+    
+    return translated_solution
 
 
-def get_denominator_coeffs(degree: int, x: np.array, f: np.array) -> np.array:
-    """Assuming f(x) = P(x) / Q(x) with P and Q polynoms of degree 'degree',
-    and y_n = f(x_n) for n in [0, len(y)]"""
-    dx = x[1:] - x[:-1]
-    assert dx.max() - dx.min() < 1e-10, f"Only support evenly spaced points"
-    assert degree > 0, "Cannot infer parameters for zero degree"
-    assert x.shape == f.shape, "You must give all pairs or (x, f(x))"
-    assert (
-        len(x) > 2 * degree
-    ), f"Need at {2 * degree} least degree points to infer the {degree} coefficients"
+def get_rationnal_fit(x, y, n, return_true_solution=True, return_residuals=False, *args, **kwargs):
+    num, den, solution, residuals = iterative_solver(x, y, n, *args, **kwargs)
+    if return_true_solution:
+        solution = convert_solution(x, solution)
 
-    dx = dx[0]
-    y = sy.symbols("y")
-    coefficients = sy.symbols(f"a:{degree+1}")
-    f_derivatives = sy.symbols(f"f:{degree+1}")
-    Q = sum([c * y**n for n, c in enumerate(coefficients)])
+    if return_residuals:
+        return solution, num/den, residuals
+    else:
+        return solution, num/den
 
-    f_Q_nth_derivative = 0 * y  # Trick so the IDE to infer it as Expr instead of int
-    # Compute in sympy
-    # \sum_{k=0}^n {n \choose k} f^{(n-k)} Q^{(k)}
-    for k in range(degree + 1):
-        term = comb(degree, k)
-        term *= f_derivatives[degree - k]
-        term *= Q.diff(y, k)
-        f_Q_nth_derivative += term
+def rationnal_function(x, coefs):
+    n = (len(coefs) // 2) - 1
 
-    # Evaluation of all derivatives of f in each point of x
-    f_numerical_derivatives = np.zeros((len(f), degree + 1))
-    f_numerical_derivatives[:, 0] = f
-    for n in range(0, degree):
-        f_numerical_derivatives[:, n + 1] = np.gradient(
-            f_numerical_derivatives[:, n], dx
-        )
+    n_mesh, x_mesh = np.meshgrid(np.arange(n+1), x)
+    x_design = (x_mesh**n_mesh).T
 
-    # we drop the points on the side since their gradient will have errors
-    x = x[degree:-degree]
-    f_numerical_derivatives = f_numerical_derivatives[degree:-degree]
+    num = x_design.T @ coefs[:n+1]
+    den = x_design.T @ coefs[n+1:]
 
-    # Build the system as a matrix
-    system = []
-    for i in range(f_numerical_derivatives.shape[0]):
-        eq = f_Q_nth_derivative.subs(y, x[i])
-        eq = eq.subs(zip(f_derivatives, f_numerical_derivatives[i]))
-        eq = sy.Poly(eq, coefficients)
-        eq = eq.coeffs()
-        eq = list(map(float, eq))
-        system.append(eq)
-    system = np.array(system)
+    return num/den
 
-    # Compute the minimizing cost
-    y = factorial(degree) * np.ones(system.shape[0])
-    (result, _residuals, _rank, _singular_values) = np.linalg.lstsq(
-        system, y, rcond=None
-    )
+def reflection_purcell(freq, f_a_0, f_b_0, kappa_a, kappa_b, g):
+    delta_a = freq - f_a_0
+    delta_b = freq - f_b_0
 
-    return result
+    num = (1j * delta_b - 0.5 * kappa_b) * (1j * delta_a + 0.5 * kappa_a) + g ** 2
+    den = (1j * delta_b + 0.5 * kappa_b) * (1j * delta_a + 0.5 * kappa_a) + g ** 2
+
+    return num / den
+
+if __name__ == "__main__":
+
+    noise = 0.1
+    n = 2
+    x = np.linspace(-5, 10, 1001)
+    coefs = np.random.rand(2*(n+1)) + 1j*np.random.rand(2*(n+1))
+    y = rationnal_function(x, coefs) + noise*np.random.normal(0, 1, (x.size)) + noise*1j*np.random.normal(0, 1, (x.size))
+
+    # y = reflection_purcell(x, -2, -1, 0.1, 3, 0.5) + noise*np.random.normal(0, 1, (x.size)) + noise*1j*np.random.normal(0, 1, (x.size))
+
+    def fit_func(x, *args):
+        n = len(args)//2
+        coefs = np.array(args[:n]) + 1j*np.array(args[n:])
+        return rationnal_function(x, coefs)
+    
+    solution, fit = get_rationnal_fit(x, y, n=n)
+    popt, pcov = complex_fit(fit_func, x, y, p0=np.array([*solution.real, *solution.imag], dtype=float))
+    better_solution = np.array(popt[:len(popt)//2]) + 1j*np.array(popt[len(popt)//2:])
+
+    print(popt)
+
+    plt.figure()
+    plt.plot(np.abs(coefs/coefs[0]), 'oC0')
+    plt.scatter(np.arange(2*(n+1)), np.abs(solution/solution[0]), facecolor='none', edgecolors='C1', zorder=100)
+    plt.scatter(np.arange(2*(n+1)), np.abs(better_solution/better_solution[0]), facecolor='none', edgecolors='C2', zorder=100)
+
+    plt.figure()
+    plt.plot(x, np.angle(y))
+    plt.plot(x, np.angle(fit))
+    plt.plot(x, np.angle(fit_func(x, *popt)))
+    plt.show()
