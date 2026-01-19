e979cf78c9483d47f8450b684ea768ad9cc94873
ureglade <ulysse.reglade@alice-bob.com>
2026-01-19 10:21:05 +0100

    feat: deployable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='abcd_rf_fit',
-    version='0.1.0',    
+    version='0.1.2',
     description='Rational function fit library',
     url='https://github.com/UlysseREGLADE/abcd_rf_fit',
     author='Ulysse REGLADE',
@@ -14,14 +14,12 @@ setup(
                       'scipy'],
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+],
 )
